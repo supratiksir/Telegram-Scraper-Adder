c8f50edc76839e0257e8bb69a9d9c0a18ce0d4c7
supratiksir <31759332+supratiksir@users.noreply.github.com>
2020-11-13 04:02:33 +0530

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -82,7 +82,7 @@ all_participants = client.get_participants(target_group, aggressive=True)
  
 print(gr+'[+] Saving In file...')
 time.sleep(1)
-with open("members.csv","w",encoding='UTF-8') as f:
+with open("/storage/emulated/0/Termuxcsv/members.csv","w",encoding='UTF-8') as f:
     writer = csv.writer(f,delimiter=",",lineterminator="\n")
     writer.writerow(['username','user id', 'access hash','name','group', 'group id'])
     for user in all_participants:
